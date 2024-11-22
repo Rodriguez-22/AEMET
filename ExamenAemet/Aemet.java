8f3a933816b7ba38181984d1bfb693cd391f0892
Rodriguez-22 <jrodrod594@g.educaand.es>
2024-11-22 09:34:33 +0100

    9:34

--- ExamenAemet/Aemet.java
+++ ExamenAemet/Aemet.java
@@ -1,5 +1,16 @@
 package ExamenAemet;
 
+import java.util.Scanner;
+
 public class Aemet {
-    
+    public static Scanner entrada = new Scanner(System.in);
+    static int NDIAS = 7;
+    static int NCIUDADES = 8;
+    static int NTEMPS = 3;
+    static String[] jrrDias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+    static String[] jrrCiudad = {"Almeria", "Granada", "Cádiz", "Malaga", "Jaén", "Cordoba", "Huelva", "Sevilla"};
+    static double[][] jrrTemperatura = 
+    public static void main(String[] args) {
+        
+    }    
 }
